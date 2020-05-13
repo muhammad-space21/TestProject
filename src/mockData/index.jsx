62aa754360435d147dbd8444e202f694e839d391
Daler Zokirov <zokirov.d@list.ru>
2020-05-14 02:29:43 +0500

    changed the menu-items images

--- src/mockData/index.jsx
+++ src/mockData/index.jsx
@@ -4,25 +4,25 @@ export const data = {
       id: 1,
       model: 'Lacetti 3-CDX A/T',
       price: 'ОТ 60 000 сум / ДЕНЬ',
-      imageUrl: 'https://hosty.xxx/i/bcca33d828a777d51d5157a6e4e62f928e7e5242.jpg'
+      imageUrl: 'https://hosty.xxx/i/e83c0fa3eb8f0f0135eca56cab15dcb3e6b91786.jpg'
     },
     {
       id: 2,
       model: 'Cobalt 4-LTZ A/T',
       price: 'ОТ 60 000 сум / ДЕНЬ',
-      imageUrl: 'https://hosty.xxx/i/b296d6eb4d699887db2f099cf712b2639fe70f97.jpg'
+      imageUrl: 'https://hosty.xxx/i/b8d5c381f584db3a5f6f16a71305256c04f1d645.jpg'
     }, 
     {
       id: 3,
       model: 'Spark 3-LT A/T',
       price: 'ОТ 60 000 сум / ДЕНЬ',
-      imageUrl: 'https://hosty.xxx/i/1100741ff2823439dc5e088453f96c9b707aa7d4.jpg'
+      imageUrl: 'https://hosty.xxx/i/7ce5e06e0dc769b0cea20afc76e0d68cb964a7e0.jpg'
     }, 
     {
       id: 4,
       model: 'Tracker A/T',
       price: 'ОТ 60 000 сум / ДЕНЬ',
-      imageUrl: 'https://hosty.xxx/i/7c1022107eca0e044303563c8bfeda5868aee387.jpg'
+      imageUrl: 'https://hosty.xxx/i/7879a59a4b0b7bcefbe5bbea88d95c1b4653f8bf.jpg'
     }
   ]
 };
