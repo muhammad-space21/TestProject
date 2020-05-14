5c7780be14d62ac86f2ca39410d367b40d318b85
Daler Zokirov <zokirov.d@list.ru>
2020-05-14 14:59:55 +0500

    made a priceTable component

--- src/pages/CalculatorPage/index.jsx
+++ src/pages/CalculatorPage/index.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import { Container } from './styles';
 
 import PricesInfo from '../../components/PricesInfo';
+import SalesTable from '../../components/PricesTable';
 
 const CalculatorPage = () => (
   <Container>
     <PricesInfo />
+    <SalesTable />
   </Container>
 );
 
